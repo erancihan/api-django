ffbaea1eb61206dd16ad034ee6991ce216ad71f5
Cihan Eran <erancihan@gmail.com>
2022-11-16 16:45:01 +0300

    handle django secret key usage

--- server/server/settings.py
+++ server/server/settings.py
@@ -10,17 +10,23 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR.parent, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4wydl++@6p$himl8n^jx_t%o2vaz$)p*&s(2f!0#f70+7cll)$'
+SECRET_KEY =  str(os.environ.get("DJANGO_SECRET_KEY", None))
+if not SECRET_KEY:
+    print("No 'DJANGO_SECRET_KEY' found in .env")
+    exit(1)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
